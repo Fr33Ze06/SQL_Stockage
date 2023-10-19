2bece0d6f71723edfea47aa732de5b1f5c1c469a
Fr33Ze06 <118484751+Fr33Ze06@users.noreply.github.com>
2023-10-19 23:28:30 +0200

    node deb

---
+++ serv.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+const port = 8080; // Port sur lequel le serveur écoutera
+
+app.get('/', (req,res) => {
+    res.redirect('/page.html');
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(port, () => {
+    console.log(`Serveur Express: http://localhost:${port}`);
+});
