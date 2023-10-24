d0eb5554aeb1e207cd26a05cd2caeb95e50a6690
Fr33Ze06 <118484751+Fr33Ze06@users.noreply.github.com>
2023-10-24 17:42:02 +0200

    ejs

--- serv.js
+++ serv.js
@@ -4,13 +4,18 @@ const path = require('path');
 const app = express();
 const port = 8080;
 
+/* EJS */ 
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
 /* Base de données SQL*/
 
 const dbConfig = {
     host: 'localhost',
-    user: 'votre_utilisateur',
-    password: 'votre_mot_de_passe',
-    database: 'votre_base_de_donnees'
+    user: 'root',
+    password: '',
+    database: 'stockage'
 };
 
 const connection = mysql.createConnection(dbConfig);
@@ -29,8 +34,17 @@ const dir = path.join(__dirname, '/');
 app.use(express.static('static'));
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(dir, 'tmpl/page.html'));
+    connection.query('SELECT entrepots.*, lieux.image AS lieux_image FROM entrepots JOIN lieux ON entrepots.id_lieu = lieux.id', (error, results) => {
+        if (error) {
+            console.error('Erreur lors de la récupération des entrepôts : ' + error.message);
+            return;
+        }
+        
+        res.render('accueil', { entrepots: results });
+    });
 });
+
+
 
 /* Page JSON */
 
@@ -38,8 +52,25 @@ app.get('/JSON', (req, res) => {
     res.sendFile(path.join(dir, 'tmpl/json.html'));
 });
 
-app.get('/JSON', (req, res) => {
-    res.sendFile(path.join(dir, 'tmpl/json.html'));
+app.get('/JSON/:jsonName', (req, res) => {
+    // Exécutez la requête SQL
+    const jsonName = req.params.jsonName;
+    select_query = 'SELECT * FROM ??';
+    tableName = jsonName;
+
+    connection.query(select_query, tableName, (error, results, fields) => {
+        if (error) {
+            console.error('Erreur lors de l\'exécution de la requête : ' + error.message);
+            return;
+        }
+
+        const jsonResponse = {
+            title: `Table de ${jsonName}`,
+            data: results
+          };
+        // Renvoie JSON
+        res.json(jsonResponse);
+    });
 });
 
 
