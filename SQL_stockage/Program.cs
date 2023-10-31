4005d4d5bb83cfc5fb594a4b162f2a62bc90e95e
Fr33Ze06 <118484751+Fr33Ze06@users.noreply.github.com>
2023-10-31 17:42:34 +0100

    cs

---
+++ SQL_stockage/Program.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SQL_stockage
+{
+    static class Program
+    {
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.SetHighDpiMode(HighDpiMode.SystemAware);
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            string connectionString = "Server=127.0.0.1;Database=stockage;User Id=root;Password=;";
+            DatabaseManager dbManager = new DbManager(connectionString);
+
+            Application.Run(new Form1(dbManager));
+        }
+    }
+}
